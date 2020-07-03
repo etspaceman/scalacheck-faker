7178372355948e020906e108ecf2e23c3724afbd
Eric Meisel <eric.steven.meisel@gmail.com>
2020-07-02 19:49:49 -0500

    Name, Internet and Time functions

--- README.md
+++ README.md
@@ -7,13 +7,16 @@ Fake Data Generation using ScalaCheck
 This library ports over common Faker routines from popular implementations, 
 such as [Java Faker](https://github.com/DiUS/java-faker) into a Scala library.
 
+# Arbitraries
 Data generators are provided in the form of [ScalaCheck Arbitraries](https://www.scala-exercises.org/scalacheck/arbitrary).
-Below is some example usage:
+In the below example, we use the instances to construct an Arbitrary instance for our own class.
 
 ```scala
-import faker._
-
 import org.scalacheck._
+
+import faker._
+// Import the default resource-loader implicits
+import faker.ResourceLoader.Implicits._
 
 final case class MyClass(firstName: String, lastName: String, emailAddress: String, ipAddress: String)
 
@@ -27,6 +30,44 @@ object MyClass {
     }  yield MyClass(firstName.value, lastName.value, email.value, ip.value)
   )
 }
+```
+The above uses `import faker.ResourceLoader.Implicits._`, which provides the default ResourceLoader for the
+Arbitrary instances. This ResourceLoader will evaluate the Faker instances against your system's default
+Locale value. You can override this value in your application's JVM 
+[through JVM args](https://docs.oracle.com/cd/E23507_01/Platform.20073/ATGProgGuide/html/s1809settingthejavavirtualmachineloca01.html).
+
+If you have a need to use a separate locale than the system default, you can create your own instance:
+
+```scala
+import java.util.Locale
+
+import org.scalacheck._
+
+import faker._
+import faker.ResourceLoader
+
+object UKResourceLoader {
+  implicit val UKResourceLoader: ResourceLoader = new ResourceLoader(Locale.UK)
+}
+```
+
+## Faker Class
+Another option for generating data is the [Faker class](src/main/scala/faker/Faker.scala). This class
+provides a similar feel to other Faker libraries.
+
+```scala
+import java.time.Instant
+import java.util.Locale
+
+import faker._
+
+val firstName: String = Faker.default.firstName()
+val nowInstant: Instant = Faker.default.nowInstant()
+
+// You can also construct a Faker instance using a manually-provided Locale:
+val ukFaker: Faker = new Faker(Locale.UK)
+
+val ukFirstName: String = ukFaker.firstName()
 ```
 
 ## Early Development Warning
--- src/main/scala/faker/Faker.scala
+++ src/main/scala/faker/Faker.scala
@@ -1,5 +1,6 @@
 package faker
 
+import java.time._
 import java.util.Locale
 
 import org.scalacheck.Arbitrary
@@ -34,6 +35,85 @@ final class Faker(private val locale: Locale) {
       .mkString("\n")
   def loremParagraphs(): String =
     Arbitrary.arbitrary[lorem.LoremParagraphs].one.value
+
+  // Name
+  def firstName(): String = Arbitrary.arbitrary[name.FirstName].one.value
+  def fullName(): String = Arbitrary.arbitrary[name.FullName].one.value
+  def fullNameWithMiddle(): String =
+    Arbitrary.arbitrary[name.FullNameWithMiddle].one.value
+  def lastName(): String = Arbitrary.arbitrary[name.LastName].one.value
+  def prefix(): String = Arbitrary.arbitrary[name.Prefix].one.value
+  def suffix(): String = Arbitrary.arbitrary[name.Suffix].one.value
+  def title(): String = Arbitrary.arbitrary[name.Title].one.value
+  def userName(): String = Arbitrary.arbitrary[name.UserName].one.value
+
+  // Internet
+  def avatar(): String = Arbitrary.arbitrary[internet.Avatar].one.value
+  def domainName(): String = Arbitrary.arbitrary[internet.DomainName].one.value
+  def domainWord(): String = Arbitrary.arbitrary[internet.DomainWord].one.value
+  def emailAddress(): String =
+    Arbitrary.arbitrary[internet.EmailAddress].one.value
+  def image(): String = Arbitrary.arbitrary[internet.Image].one.value
+  def ipV4Address(): String =
+    Arbitrary.arbitrary[internet.IpV4Address].one.value
+  def ipV4Cidr(): String = Arbitrary.arbitrary[internet.IpV4Cidr].one.value
+  def ipV6Address(): String =
+    Arbitrary.arbitrary[internet.IpV6Address].one.value
+  def ipV6Cidr(): String = Arbitrary.arbitrary[internet.IpV6Cidr].one.value
+  def macAddress(): String = Arbitrary.arbitrary[internet.MacAddress].one.value
+  def password(): String = Arbitrary.arbitrary[internet.Password].one.value
+  def privateIpV4Address(): String =
+    Arbitrary.arbitrary[internet.PrivateIpV4Address].one.value
+  def publicIpV4Address(): String =
+    Arbitrary.arbitrary[internet.PublicIpV4Address].one.value
+  def safeEmailAddress(): String =
+    Arbitrary.arbitrary[internet.SafeEmailAddress].one.value
+  def slug(): String = Arbitrary.arbitrary[internet.Slug].one.value
+  def url(): String = Arbitrary.arbitrary[internet.Url].one.value
+  def userAgent(): String = Arbitrary.arbitrary[internet.UserAgent].one.value
+
+  // Time
+  def currentEraInstant(): Instant =
+    Arbitrary.arbitrary[time.CurrentEraInstant].one.value
+  def currentEraLocalDateTime(): LocalDateTime =
+    Arbitrary.arbitrary[time.CurrentEraLocalDateTime].one.value
+  def currentEraOffsetDateTime(): OffsetDateTime =
+    Arbitrary.arbitrary[time.CurrentEraOffsetDateTime].one.value
+  def currentEraZonedDateTime(): ZonedDateTime =
+    Arbitrary.arbitrary[time.CurrentEraZonedDateTime].one.value
+  def futureInstant(): Instant =
+    Arbitrary.arbitrary[time.FutureInstant].one.value
+  def futureLocalDateTime(): LocalDateTime =
+    Arbitrary.arbitrary[time.FutureLocalDateTime].one.value
+  def futureOffsetDateTime(): OffsetDateTime =
+    Arbitrary.arbitrary[time.FutureOffsetDateTime].one.value
+  def futureZonedDateTime(): ZonedDateTime =
+    Arbitrary.arbitrary[time.FutureZonedDateTime].one.value
+  def nowInstant(): Instant =
+    Arbitrary.arbitrary[time.NowInstant].one.value
+  def nowLocalDateTime(): LocalDateTime =
+    Arbitrary.arbitrary[time.NowLocalDateTime].one.value
+  def nowOffsetDateTime(): OffsetDateTime =
+    Arbitrary.arbitrary[time.NowOffsetDateTime].one.value
+  def nowZonedDateTime(): ZonedDateTime =
+    Arbitrary.arbitrary[time.NowZonedDateTime].one.value
+  def pastInstant(): Instant =
+    Arbitrary.arbitrary[time.PastInstant].one.value
+  def pastLocalDateTime(): LocalDateTime =
+    Arbitrary.arbitrary[time.PastLocalDateTime].one.value
+  def pastOffsetDateTime(): OffsetDateTime =
+    Arbitrary.arbitrary[time.PastOffsetDateTime].one.value
+  def pastZonedDateTime(): ZonedDateTime =
+    Arbitrary.arbitrary[time.PastZonedDateTime].one.value
+  def randomInstant(): Instant =
+    Arbitrary.arbitrary[time.RandomInstant].one.value
+  def randomLocalDateTime(): LocalDateTime =
+    Arbitrary.arbitrary[time.RandomLocalDateTime].one.value
+  def randomOffsetDateTime(): OffsetDateTime =
+    Arbitrary.arbitrary[time.RandomOffsetDateTime].one.value
+  def randomZonedDateTime(): ZonedDateTime =
+    Arbitrary.arbitrary[time.RandomZonedDateTime].one.value
+
 }
 
 object Faker {
