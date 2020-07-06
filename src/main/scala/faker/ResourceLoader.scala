72be1bf2f09f47ce730950bb86466109d21ff29c
Eric Meisel <eric.steven.meisel@gmail.com>
2020-07-05 22:36:34 -0500

    Update resource loading

--- src/main/scala/faker/ResourceLoader.scala
+++ src/main/scala/faker/ResourceLoader.scala
@@ -7,19 +7,33 @@ import java.util.Locale
 import pureconfig._
 
 final class ResourceLoader(private val locale: Locale) {
+  private val country: Option[String] = {
+    val cty = Option(locale.getCountry)
+    if (cty.exists(_.isEmpty)) None else cty
+  }
+  private val language: Option[String] = {
+    val lang = Option(locale.getLanguage)
+    if (lang.exists(_.isEmpty)) None else lang
+  }
 
   private val defaultConfig: ConfigObjectSource =
     ConfigSource.resources("default.conf")
-  private val languageConfig: ConfigObjectSource =
-    ConfigSource.resources(s"${locale.getLanguage}.conf")
-  private val localeConfig: ConfigObjectSource =
-    ConfigSource.resources(s"${locale.toString}.conf")
+  private val languageConfig: Option[ConfigObjectSource] =
+    language.map(x => ConfigSource.resources(s"$x.conf"))
+  private val localeConfig: Option[ConfigObjectSource] = for {
+    lang <- language
+    cty <- country
+  } yield ConfigSource.resources(s"${lang}_$cty.conf")
 
   // Fallback Pattern: en-US.conf -> en.conf -> default.conf
-  private val conf: ConfigSource =
-    localeConfig.optional
-      .withFallback(languageConfig.optional)
-      .withFallback(defaultConfig)
+  private val conf: ConfigSource = (languageConfig, localeConfig) match {
+    case (Some(langConf), Some(localeConf)) =>
+      localeConf.optional
+        .withFallback(langConf.optional)
+        .withFallback(defaultConfig)
+    case (Some(langConf), _) => langConf.optional.withFallback(defaultConfig)
+    case _                   => defaultConfig
+  }
 
   def loadKey[A: ConfigReader: ClassTag](key: String): A =
     conf.at(key).loadOrThrow[A]
