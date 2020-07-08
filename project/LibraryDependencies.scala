53ae63eaf9a68414f07f51d1ef9bd5be1779a225
Scala Steward <me@scala-steward.org>
2020-07-08 02:16:50 +0200

    Update scalacheck-1-14 to 3.2.0.0

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -6,7 +6,7 @@ object LibraryDependencies {
   val ApacheCommons = "org.apache.commons" % "commons-lang3" % "3.10"
   val ScalaTest = "org.scalatest" %% "scalatest" % "3.2.0"
   val ScalaTestPlusScalaCheck =
-    "org.scalatestplus" %% "scalacheck-1-14" % "3.1.2.0"
+    "org.scalatestplus" %% "scalacheck-1-14" % "3.2.0.0"
   val KindProjector = "org.typelevel" % "kind-projector" % "0.11.0"
   val OrganizeImports =
     "com.github.liancheng" %% "organize-imports" % "0.3.1-RC3"
