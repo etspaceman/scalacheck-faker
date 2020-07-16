946d8f91a9ee82ad24f34db95c20ddcdd91603b2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-07-16 17:01:19 +0200

    Update commons-lang3 to 3.11 (#55)

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object LibraryDependencies {
   val ScalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.3"
   val TypesafeConfig = "com.typesafe" % "config" % "1.4.0"
-  val ApacheCommons = "org.apache.commons" % "commons-lang3" % "3.10"
+  val ApacheCommons = "org.apache.commons" % "commons-lang3" % "3.11"
   val ScalaTest = "org.scalatest" %% "scalatest" % "3.2.0"
   val ScalaTestPlusScalaCheck =
     "org.scalatestplus" %% "scalacheck-1-14" % "3.2.0.0"
