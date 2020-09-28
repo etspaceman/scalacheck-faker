25f575dae1cb8b515fcb1e190a81ba1110d0e599
Scala Steward <me@scala-steward.org>
2020-09-28 02:15:29 +0200

    Update organize-imports to 0.4.2

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -9,7 +9,7 @@ object LibraryDependencies {
     "org.scalatestplus" %% "scalacheck-1-14" % "3.2.2.0"
   val KindProjector = "org.typelevel" % "kind-projector" % "0.11.0"
   val OrganizeImports =
-    "com.github.liancheng" %% "organize-imports" % "0.3.1-RC3"
+    "com.github.liancheng" %% "organize-imports" % "0.4.2"
   val PureConfig = "com.github.pureconfig" %% "pureconfig" % "0.14.0"
   val ScalaCheckGenRegexp = "wolfendale" %% "scalacheck-gen-regexp" % "0.1.2"
 }
