a799bc2f067f6d7da60013b4470be4d9359ab7fe
Eric Meisel <eric.steven.meisel@gmail.com>
2020-11-09 09:46:13 -0600

    Fix compile

--- src/main/scala/faker/address/BuildingNumber.scala
+++ src/main/scala/faker/address/BuildingNumber.scala
@@ -1,7 +1,5 @@
 package faker.address
 
-import scala.jdk.CollectionConverters._
-
 import org.scalacheck.{Arbitrary, Gen}
 
 import faker.ResourceLoader
@@ -15,11 +13,11 @@ object BuildingNumber {
   ): Arbitrary[BuildingNumber] =
     Arbitrary(
       Gen
-        .sequence(
+        .sequence[Seq[String], String](
           loader
             .loadKey[Seq[String]]("address.building-numbers")
             .map(_.interpolatedGen)
         )
-        .flatMap(x => Gen.oneOf(x.asScala).map(BuildingNumber.apply))
+        .flatMap(x => Gen.oneOf(x).map(BuildingNumber.apply))
     )
 }
