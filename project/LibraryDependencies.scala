ab64d4bae4ae199ffc10aa58ae6c29094ae32c2b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-02-01 12:34:36 +0100

    Update organize-imports to 0.5.0 (#145)

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -9,7 +9,7 @@ object LibraryDependencies {
     "org.scalatestplus" %% "scalacheck-1-14" % "3.2.2.0"
   val KindProjector = "org.typelevel" % "kind-projector" % "0.11.3"
   val OrganizeImports =
-    "com.github.liancheng" %% "organize-imports" % "0.4.4"
+    "com.github.liancheng" %% "organize-imports" % "0.5.0"
   val PureConfig = "com.github.pureconfig" %% "pureconfig" % "0.14.0"
   val ScalaCheckGenRegexp = "wolfendale" %% "scalacheck-gen-regexp" % "0.1.2"
   val NewType = "io.estatico" %% "newtype" % "0.4.4"
