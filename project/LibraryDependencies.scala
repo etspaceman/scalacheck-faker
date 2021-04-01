1a67dfa5918af8c07892135d1ac92e37fd06f038
Scala Steward <me@scala-steward.org>
2021-04-01 08:21:16 +0200

    Update scalatest to 3.2.7

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -4,7 +4,7 @@ object LibraryDependencies {
   val ScalaCheck = "org.scalacheck" %% "scalacheck" % "1.15.2"
   val TypesafeConfig = "com.typesafe" % "config" % "1.4.1"
   val ApacheCommons = "org.apache.commons" % "commons-lang3" % "3.12.0"
-  val ScalaTest = "org.scalatest" %% "scalatest" % "3.2.6"
+  val ScalaTest = "org.scalatest" %% "scalatest" % "3.2.7"
   val ScalaTestPlusScalaCheck =
     "org.scalatestplus" %% "scalacheck-1-14" % "3.2.2.0"
   val KindProjector = "org.typelevel" % "kind-projector" % "0.11.3"
