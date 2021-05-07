b00f98d48039b5ae1fa5af814b46a78549e7310d
Scala Steward <me@scala-steward.org>
2021-05-07 06:17:05 +0200

    Update pureconfig to 0.15.0

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -10,7 +10,7 @@ object LibraryDependencies {
   val KindProjector = "org.typelevel" % "kind-projector" % "0.11.3"
   val OrganizeImports =
     "com.github.liancheng" %% "organize-imports" % "0.5.0"
-  val PureConfig = "com.github.pureconfig" %% "pureconfig" % "0.14.1"
+  val PureConfig = "com.github.pureconfig" %% "pureconfig" % "0.15.0"
   val ScalaCheckGenRegexp =
     "io.github.wolfendale" %% "scalacheck-gen-regexp" % "0.1.3"
   val NewType = "io.estatico" %% "newtype" % "0.4.4"
