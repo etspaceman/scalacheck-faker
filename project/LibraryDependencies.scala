a558cc4c250938e5b14bd806c0990770ba491ea6
Scala Steward <scala_steward@virtuslab.com>
2023-05-24 19:46:53 +0000

    Update scalacheck-1-15 to 3.2.14.0

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -6,7 +6,7 @@ object LibraryDependencies {
   val ApacheCommons = "org.apache.commons" % "commons-lang3" % "3.12.0"
   val ScalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
   val ScalaTestPlusScalaCheck =
-    "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0"
+    "org.scalatestplus" %% "scalacheck-1-16" % "3.2.14.0"
   val KindProjector = "org.typelevel" % "kind-projector" % "0.13.2"
   val OrganizeImports =
     "com.github.liancheng" %% "organize-imports" % "0.6.0"
