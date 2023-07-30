b96da505c83a8a8cbb818caf83acd403a25ea401
Eric Meisel <eric.steven.meisel@gmail.com>
2023-07-29 23:30:02 -0500

    fix

--- src/test/scala/faker/FakerSpec.scala
+++ src/test/scala/faker/FakerSpec.scala
@@ -69,6 +69,5 @@ trait FakerSpec extends SimpleIOSuite with Checkers {
         } yield res1 && res2
       }
       .map(_.foldLeft(Expectations(Valid(()))) { case (a, x) => a && x })
-    }
   }
 }
